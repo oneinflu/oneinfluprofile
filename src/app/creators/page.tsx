6e23645ed9c4e2c0c46d204785f1dab5dec61a9c
oneinflu <hashtaginflu@gmail.com>
2026-01-22 10:53:35 +0530

    style(creators): remove redundant padding from main container

--- src/app/creators/page.tsx
+++ src/app/creators/page.tsx
@@ -5,7 +5,7 @@ import { CreatorFeatures } from "@/components/marketing/creator-features";
 
 export default function CreatorsPage() {
     return (
-        <main className="min-h-screen pt-0 pb-20">
+        <main className="min-h-screen ">
             <CreatorsHero />
             <CreatorChat />
             <CreatorFeatures />
